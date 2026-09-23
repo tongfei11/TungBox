29e55e601b78d1cb1ee0eece04ac1c478bb826c4
tongfei11 <tongfei1991@gmail.com>
2026-09-23 11:14:40 +0800

    chore: release 0.3.1 (0222)

--- Sources/TungBox/Core/AppMetadata.swift
+++ Sources/TungBox/Core/AppMetadata.swift
@@ -2,8 +2,8 @@ import AppKit
 import Foundation
 
 enum TungBoxVersion {
-    static let release = "0.3.0"
-    static let build = "0221"
+    static let release = "0.3.1"
+    static let build = "0222"
     static let current = "\(release)(\(build))"
     static let display = "TungBox v\(current)"
 }
